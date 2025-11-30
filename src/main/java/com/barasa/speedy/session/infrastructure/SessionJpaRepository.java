89b6d7f327bf5a3dd32c035f6fdfb44e465a7b0f
barasapeter <barasapeter52@gmail.com>
2025-11-30 18:56:50 +0300

    Added JpaRepositories for all domains

--- src/main/java/com/barasa/speedy/session/infrastructure/SessionJpaRepository.java
+++ src/main/java/com/barasa/speedy/session/infrastructure/SessionJpaRepository.java
@@ -1,11 +1,10 @@
 package com.barasa.speedy.session.infrastructure;
 
 import com.barasa.speedy.bike.infrastructure.BikeEntity;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
-public interface BikeJpaRepository extends JpaRepository<BikeEntity, String> {
+public interface SessionJpaRepository extends JpaRepository<SessionEntity, UUID> {
     List<BikeEntity> findByShopUuid(UUID shopUuid);
 }

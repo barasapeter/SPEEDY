bd63e62f8795368828b27077902250b2300736e4
barasapeter <barasapeter52@gmail.com>
2025-11-30 17:22:30 +0300

    Keep creating entities [Tables]: do not forget to resolve addinfo into jsondb

--- src/main/java/com/barasa/speedy/shop/infrastructure/ShopEntity.java
+++ src/main/java/com/barasa/speedy/shop/infrastructure/ShopEntity.java
@@ -1,5 +1,37 @@
 package com.barasa.speedy.shop.infrastructure;
 
+import com.barasa.speedy.user.infrastructure.UserEntity;
+import com.barasa.speedy.bike.infrastructure.BikeEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "shop")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShopEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "UUID")
+    private UUID uuid;
+
+    private String name;
+
+    private String location;
+
+    @Column(columnDefinition = "TEXT")
+    private String addinfo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner", nullable = false)
+    private UserEntity owner;
+
+    @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BikeEntity> inventory;
 }
--- src/main/java/com/barasa/speedy/user/infrastructure/UserEntity.java
+++ src/main/java/com/barasa/speedy/user/infrastructure/UserEntity.java
@@ -1,5 +1,34 @@
 package com.barasa.speedy.user.infrastructure;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "UUID")
+    private UUID uuid;
+
+    private String name;
+
+    private String phone;
+
+    @Column(columnDefinition = "TEXT")
+    private String addinfo;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ShopEntity> ownedShops;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<SessionEntity> sessions;
 }
